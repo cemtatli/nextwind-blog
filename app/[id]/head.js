d15ec32227cd903fc0eaf8f7f1101a266c6ee7aa
Cem <cemtatli3535@gmail.com>
2023-03-09 13:34:09 +0300

    custom comments added to each blog

---
+++ app/[id]/head.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Head({}) {
+  return (
+    <>
+      <title>Blog Content </title>
+    </>
+  );
+}
--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import React from "react";
-import { HeartIcon } from "@heroicons/react/24/solid";
+import { HeartIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
 
 async function getPost(id) {
   // id parametresi alıyoruz.
@@ -18,51 +18,119 @@ export default async function BlogPage({ params }) {
 
   return (
     <div className="flex items-center justify-center w-full gap-4 text-center flex-col px-4 py-2 mt-10">
-      <div className="max-w-4xl relative w-full h-60">
-        <Image
-          src={`https://picsum.photos/960/400?random=${params.id}`}
-          alt={title}
-          className="rounded-lg object-cover"
-          fill
-        />
-      </div>
-      <div className="flex items-center justify-center w-full max-w-4xl p-2 space-x-4">
-        <div className="flex items-center justify-center space-x-2">
+      <>
+        <div className="max-w-4xl relative w-full h-60">
           <Image
-            src={`https://i.pravatar.cc/150?img=${userId}}`}
+            src={`https://picsum.photos/960/400?random=${params.id}`}
             alt={title}
-            className="rounded-full object-cover"
-            width={50}
-            height={50}
+            className="rounded-lg object-cover relative"
+            fill
           />
-        </div>
-        <span className="text-sm text-main-gray dark:text-main-white font-medium">
-          {new Date().toLocaleDateString()}
-        </span>
-        <span className="text-sm text-main-gray dark:text-main-white font-medium">
-          {new Date().toLocaleTimeString()}
-        </span>
-        <span className="text-sm text-main-gray dark:text-main-white font-medium flex items-center ">
-          {userId} <HeartIcon className="h-5 w-5 text-red-500 ml-0.5 cursor-pointer" />
-        </span>
-      </div>
-      <div className="flex items-center justify-center w-full max-w-4xl p-2 space-x-4">
-        {tags.map((tag) => (
-          <span
-            key={id}
-            className="text-xs cursor-pointer text-main-gray dark:text-main-white font-medium bg-main-gray/10 dark:bg-white/10 px-3 py-1 rounded-xl hover:bg-main-blue hover:text-main-white transition-all duration-300 ease-in-out"
-          >
-            {tag}
+          <span className="text-xs bottom-2 left-2  text-main-white dark:text-main-white font-medium  bg-main-gray/10  dark:bg-white/10 px-3 py-1 rounded-xl absolute">
+            {reactions} min read
           </span>
-        ))}
-        <span className="text-xs text-main-gray dark:text-main-white font-medium bg-main-gray/10 dark:bg-white/10 px-3 py-1 rounded-xl">
-          {reactions} min read
-        </span>
-      </div>
+        </div>
+        <div className="flex items-center justify-center w-full max-w-4xl space-x-4 py-2">
+          <div className="flex items-center justify-center space-x-2">
+            <Image
+              src={`https://i.pravatar.cc/150?img=${userId}}`}
+              alt={title}
+              className="rounded-full object-cover "
+              width={50}
+              height={50}
+            />
+          </div>
+          <span className="text-sm text-main-gray dark:text-main-white font-medium">
+            {new Date().toLocaleDateString()}
+          </span>
+          <span className="text-sm text-main-gray dark:text-main-white font-medium">
+            {new Date().toLocaleTimeString()}
+          </span>
+          <span className="text-sm text-main-gray dark:text-main-white font-medium flex items-center ">
+            {userId}{" "}
+            <HeartIcon width={14} height={14} className=" text-red-500 ml-0.5 cursor-pointer" />
+          </span>
+        </div>
+        <div className="flex items-center justify-center w-full max-w-4xl space-x-4">
+          {tags.map((tag) => (
+            <span
+              key={id}
+              className="text-xs cursor-pointer text-main-gray dark:text-main-white font-medium bg-main-gray/10 dark:bg-white/10 px-3 py-1 rounded-xl hover:bg-main-blue hover:text-main-white transition-all duration-300 ease-in-out"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="flex flex-col items-center justify-center w-full max-w-4xl p-4 space-y-4">
+          <h1 className="text-4xl font-bold text-main-black dark:text-main-white">{title}</h1>
+          <p className="text-gray-500 ">{body}</p>
+        </div>
+      </>
+      <div className="my-10"></div>
+      {/* Comment */}
+      <div className="flex flex-col cursor-pointer items-center justify-center w-full mb-10 max-w-2xl p-4 space-y-4 border-t border-main-black border-opacity-10  dark:border-main-white/10 ">
+        <div className="flex flex-col items-center justify-center w-full mt-4 max-w-4xl p-2 space-x-4 space-y-2 hover:bg-main-gray/10 dark:hover:bg-white/10 rounded-xl group transition-all duration-300 ease-in-out">
+          <div className="flex items-center justify-center space-x-2">
+            <Image
+              src={`https://i.pravatar.cc/150?img=1${id} }`}
+              alt={title}
+              className="rounded-full object-cover "
+              width={40}
+              height={40}
+            />
+            <span className="text-xs text-main-gray dark:text-main-white font-medium">
+              {new Date().toLocaleDateString()}
+            </span>
+            <span className="text-xs text-main-gray dark:text-main-white font-medium">
+              {new Date().toLocaleTimeString()}
+            </span>
 
-      <div className="flex flex-col items-center justify-center w-full max-w-4xl p-4 space-y-4">
-        <h1 className="text-4xl font-bold text-main-black dark:text-main-white">{title}</h1>
-        <p className="text-gray-500 ">{body}</p>
+            <span className="text-sm text-main-gray dark:text-main-white font-medium flex items-center ">
+              {userId}{" "}
+              <HandThumbUpIcon
+                width={14}
+                height={14}
+                className=" ml-1 cursor-pointer hover:scale-125"
+              />
+            </span>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <span className="text-sm text-main-gray dark:text-main-white font-medium line-clamp-1">
+              {body}
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full mt-4 max-w-4xl p-2 space-x-4 space-y-2 hover:bg-main-gray/10 dark:hover:bg-white/10 rounded-xl group transition-all duration-300 ease-in-out">
+          <div className="flex items-center justify-center space-x-2">
+            <Image
+              src={`https://i.pravatar.cc/150?img=${id} }`}
+              alt={title}
+              className="rounded-full object-cover "
+              width={40}
+              height={40}
+            />
+            <span className="text-xs text-main-gray dark:text-main-white font-medium">
+              {new Date().toLocaleDateString()}
+            </span>
+            <span className="text-xs text-main-gray dark:text-main-white font-medium">
+              {new Date().toLocaleTimeString()}
+            </span>
+
+            <span className="text-sm text-main-gray dark:text-main-white font-medium flex items-center ">
+              {id}
+              <HandThumbUpIcon
+                width={14}
+                height={14}
+                className=" ml-1 cursor-pointer hover:scale-125"
+              />
+            </span>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <span className="text-sm text-main-gray dark:text-main-white font-medium line-clamp-1">
+              {body}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
